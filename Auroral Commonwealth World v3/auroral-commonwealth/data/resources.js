98fd0bee4d654a212a68793ad3102922cbe92f8f
SupremeGenZ-Github <gamingmoneesh@gmail.com>
2026-08-30 20:11:20 +0530

    Add files via upload

---
+++ Auroral Commonwealth World v3/auroral-commonwealth/data/resources.js
@@ -0,0 +1,6 @@
+export const RESOURCE_DEFS = {
+  oil:{name:'Oil',basePrice:78,unit:'M bbl'}, gas:{name:'Natural Gas',basePrice:42,unit:'B m³'}, coal:{name:'Coal',basePrice:25,unit:'Mt'},
+  iron:{name:'Iron',basePrice:110,unit:'Mt'}, copper:{name:'Copper',basePrice:160,unit:'kt'}, uranium:{name:'Uranium',basePrice:210,unit:'kt'},
+  rareEarths:{name:'Rare Earths',basePrice:340,unit:'kt'}, lithium:{name:'Lithium',basePrice:280,unit:'kt'}, food:{name:'Food',basePrice:65,unit:'index'},
+  water:{name:'Water',basePrice:20,unit:'index'}, timber:{name:'Timber',basePrice:70,unit:'Mt'}
+};
